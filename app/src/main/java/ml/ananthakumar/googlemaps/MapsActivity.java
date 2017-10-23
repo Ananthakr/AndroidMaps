b1a80a835f852aabc3d1f0fbc9c9c0f16e13c393
theMachine <ananthakumartpmstar@gmail.com>
2017-10-23 21:03:34 +0530

    Initial commit

---
+++ app/src/main/java/ml/ananthakumar/googlemaps/MapsActivity.java
@@ -0,0 +1,46 @@
+package ml.ananthakumar.googlemaps;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng ceg = new LatLng(13.010920, 80.235491);
+        mMap.addMarker(new MarkerOptions()
+                .position(ceg)
+                .title("CEG, Anna University")
+                .snippet("Sardar Patel Road, Guindy, Anna University, Kotturpuram, Chennai, Tamil Nadu 600025.")
+        );
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ceg,17));
+
+        //Use this to change Map type
+        // Other supported types include: MAP_TYPE_NORMAL,
+        // MAP_TYPE_TERRAIN, MAP_TYPE_HYBRID and MAP_TYPE_NONE
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+    }
+}
